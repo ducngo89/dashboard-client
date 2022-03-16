a9105daa8422771627421db890b2facb0ddb168b
ducngo89 <ducngo89@gmail.com>
2022-03-16 22:33:47 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  console.debug("[dashboard-client] loaded");
+  return (
+    <div id="Dashboard-Client">
+      <p>This is the dashboard-client</p>
+    </div>
+  );
+}
